6b11901e00ab431b0eac5cbfc554ebd037d8e78e
fabio-araujo-silva <fabio.kaue.araujo@gmail.com>
2023-09-01 10:06:15 -0300

    Construindo base

--- arq1.py
+++ arq1.py
@@ -1,1 +1,26 @@
-print("teste1")
+import pygame
+from pygame.locals import *
+from sys import exit
+
+#inicializa o pygame
+pygame.init()
+
+#muda o nome da janela do jogo
+pygame.display.set_caption('breakout2')
+
+#seta o tamano da tela
+width = 640
+height = 480
+screen = pygame.display.set_mode((width, height))
+
+#loop principal do jogo
+while True:
+    for event in pygame.event.get():
+        
+        #configura botao de saída
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+        
+        #faz o jogo sempre atualizar
+        pygame.display.update()
