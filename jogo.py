e9d6e1f269df5a4ec0b052d5bcf295642e953e55
fabio-araujo-silva <fabio.kaue.araujo@gmail.com>
2023-09-02 23:08:57 -0300

    update no codigo

--- jogo.py
+++ jogo.py
@@ -11,10 +11,22 @@ pygame.display.set_caption('breakout2')
 #seta o tamano da tela
 width = 640
 height = 480
+
+#seta posição inicial do bloco
+x=height/2
+y=0
 screen = pygame.display.set_mode((width, height))
+
+clock = pygame.time.Clock()
 
 #loop principal do jogo
 while True:
+
+    #framerate
+    clock.tick(30)
+    #"limpa tela"
+    screen.fill((0,0,0))
+
     for event in pygame.event.get():
         
         #configura botao de saída
@@ -22,6 +34,11 @@ while True:
             pygame.quit()
             exit()
         
-        pygame.draw.rect(tela, ())
+        #adiciona bloco branco
+        pygame.draw.rect(screen, (255,255,255),(x,y,40,40))
+        if y>=height:
+            y=0
+        y=y+10
+
         #faz o jogo sempre atualizar
         pygame.display.update()
